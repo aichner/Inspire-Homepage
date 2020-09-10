8c19143ed4d9abf07c28235580eea248b2ef90df
Luca Allmaier <allmaier@aichner.cloud>
2020-09-10 08:42:06 +0200

    Improve sections exports
    The new sections have been added to the section exports.

---
+++ src/components/organisms/sections/index.js
@@ -0,0 +1,10 @@
+//> Components
+// Import all components to export them for easy access from parent components
+import CallToAction from "./CallToAction";
+import Reviews from "./Reviews";
+
+export { CallToAction, Reviews };
+/**
+ * SPDX-License-Identifier: (EUPL-1.2)
+ * Copyright © 2020 Werbeagentur Christian Aichner
+ */
